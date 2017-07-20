401337b83af9c616e1f05260994c6556ed24e148
James Fonzone <jfonzone@outlook.com>
2017-07-20 17:43:54 -0400

    NPCPatrol script

---
+++ Assets/Scripts/NPCPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPatrol : MonoBehaviour {
+	NavMeshAgent npcAgent;
+	Vector3 point_A = new Vector3(0f,0f,0f);
+ 	Vector3 point_B = new Vector3(5f,0f,0f);
+
+	void Start()
+	{
+		npcAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		npcAgent.destination = point_A;
+		if(gameObject.transform.position == point_A)
+		{
+			Debug.Log ("Reached point A");
+			npcAgent.destination = point_B;
+		}
+	}
+}
