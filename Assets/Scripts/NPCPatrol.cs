043006fd29a5c39e94fde05d9956be63b827f54e
James Fonzone <jfonzone@outlook.com>
2017-07-20 17:52:15 -0400

    fix buggy movement

--- Assets/Scripts/NPCPatrol.cs
+++ Assets/Scripts/NPCPatrol.cs
@@ -6,7 +6,6 @@ using UnityEngine.AI;
 public class NPCPatrol : MonoBehaviour {
 	NavMeshAgent npcAgent;
 	Vector3 point_A = new Vector3(0f,0f,0f);
- 	Vector3 point_B = new Vector3(5f,0f,0f);
 
 	void Start()
 	{
@@ -16,10 +15,5 @@ public class NPCPatrol : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		npcAgent.destination = point_A;
-		if(gameObject.transform.position == point_A)
-		{
-			Debug.Log ("Reached point A");
-			npcAgent.destination = point_B;
-		}
 	}
 }
